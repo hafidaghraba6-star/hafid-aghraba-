6e3f89408066d1a434ea1eee485bfc9501514e45
hafidaghraba6-star <hafidaghraba6@gmail.com>
2026-01-28 14:16:10 +0100

    test commit

---
+++ exercice5.py
@@ -0,0 +1,11 @@
+def trouver_maximum(nombres):
+    """
+    Trouve et retourne le plus grand élément d'une liste de nombres.
+    
+    Args:
+        nombres: Une liste de nombres
+        
+    Returns:
+        Le plus grand élément de la liste
+    """
+    return max(nombres)
